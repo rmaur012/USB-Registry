0895af922bd486730e5dd5c9a24df828ab90dfe2
Ricardo Maury <rmaur012@fiu.edu>
2018-01-22 16:07:26 -0500

    Moved all non-main functions to USBRegOps.py. Fixed a bug that would not read the file properly to see if a device is already registered. Minor additions to function parameters to be able to pass the list of port numbers.

---
+++ USBRegOps.py
@@ -0,0 +1,110 @@
+import sys
+import os
+import usb.core
+import usb.util
+import serial
+
+#To prevent the 'Error: The device has no langid' issue
+#Must add the line below to the 99-*.rules file at: /etc/udev/rules.d
+#SUBSYSTEM=="usb", MODE=="0666", GROUP=="usbusers"
+
+#Open the USBLog and clear out the log
+def clearLog():
+    try:
+        if os.stat('USBLog').st_size:
+            open('USBLog', 'w').close()
+            print("Log has been cleared!")
+        else:
+            print("Nothing to clear!")
+    except:
+        raise
+
+#Display each entry in the log
+def displayLog():
+    try:
+        logFile = open('USBLog', 'r')
+        logData = list(logFile)
+
+        #If there is at least 1 entry in the log, display all entries
+        if len(logData):
+            print("ID:Product:Manufacturer")
+            print("____Log Entries___")
+            for log in logData:
+                print(log.rstrip('\n'))
+        #Else, tell the user there are no entries
+        else:
+            print("The log is empty!")
+
+    except:
+        raise
+    finally:
+        logFile.close()
+
+
+#Port numbers are passed from main
+def logAndSeeConnectedDevices(devs, PORT_NUMBERS):
+    USBCount = 0
+    try:
+        logFile = open('USBLog', 'a+')
+        logFile.seek(0)
+        logData = logFile.readlines()
+        for dev in devs:
+            FoundID = False
+            #USB ports have port numbers 2, 3, 4 &  5
+            if dev.port_number in PORT_NUMBERS:
+                
+                #1. Search Log to see if this device can be found in it. The ID is in the format: idVendor$idProduct
+                DevLogID = str(dev.idProduct) + '^' + str(dev.idVendor)
+                for line in logData:
+                    logParts = line.split(":")
+                    lineID = logParts[0]
+                
+                    #2. If device is found, display and state that it is registered.
+                    if lineID == DevLogID:
+                        FoundID = True
+                        print(str(dev.product) + ' by '  + str(dev.manufacturer) + ' connected at USB port ' + str(dev.port_number)  + ". Device Already Logged.")
+                        break
+                            
+                    #3. If not, register the device and display it. Noting that is has been saved. The format is= ID:Product:Vendor
+                if not FoundID:
+                    newEntry = DevLogID + ":" + dev.product + ":" + dev.manufacturer + "\n"
+                    logFile.write(newEntry)
+                    print(str(dev.product) + ' by ' + str(dev.manufacturer) + ' connected at USB port ' + str(dev.port_number) + ". Device Now Registered.")
+        #           logData = logFile.readLines()
+                
+                USBCount = USBCount + 1
+
+
+        if not USBCount:
+            print("Nothing connected to USB Ports!")
+    
+    except:
+        raise
+
+    finally:
+        logFile.close()
+
+def callParamTree(param, devs, PORT_NUMBERS):
+
+    if param is None:
+        logAndSeeConnectedDevices(devs, PORT_NUMBERS)
+
+    elif param == "-d":
+        displayLog()
+
+    elif param == "-ld":
+        logAndSeeConnectedDevices(devs, PORT_NUMBERS)
+        displayLog()
+    
+    elif param == "-c":
+        clearLog()
+    
+    elif param == "-dc":
+        displayLog()
+        clearLog()
+    
+    elif param == "-ldc":
+        logAndSeeConnectedDevices(devs, PORT_NUMBERS)
+        displayLog()
+        clearLog()
+
--- USBRegistry.py
+++ USBRegistry.py
@@ -1,110 +1,15 @@
 import sys
 import os
 import usb.core
-import usb.util
-import serial
+
+#sys.path.insert(0, os.path.realpath('./USBRegOps.py'))
+import USBRegOps
 
 #To prevent the 'Error: The device has no langid' issue
 #Must add the line below to the 99-*.rules file at: /etc/udev/rules.d
 #SUBSYSTEM=="usb", MODE=="0666", GROUP=="usbusers"
 
-#Open the USBLog and clear out the log
-def clearLog():
-    try:
-        if os.stat('USBLog').st_size:
-            open('USBLog', 'w').close()
-            print("Log has been cleared!")
-        else:
-            print("Nothing to clear!")
-    except:
-        raise
-
-#Display each entry in the log
-def displayLog():
-    try:
-        logFile = open('USBLog', 'r')
-        logData = list(logFile)
-
-        #If there is at least 1 entry in the log, display all entries
-        if len(logData):
-            print("ID:Product:Manufacturer")
-            print("____Log Entries___")
-            for log in logData:
-                print(log.rstrip('\n'))
-        #Else, tell the user there are no entries
-        else:
-            print("The log is empty!")
-
-    except:
-        raise
-    finally:
-        logFile.close()
-
-
-def logAndSeeConnectedDevices(devs):
-    USBCount = 0
-    try:
-        logFile = open('USBLog', 'a+')
-        logData = logFile.readlines()
-        for dev in devs:
-            FoundID = False
-            #USB ports have port numbers 2, 3, 4 &  5
-            if dev.port_number in PORT_NUMBERS:
-                
-                #1. Search Log to see if this device can be found in it. The ID is in the format: idVendor$idProduct
-                DevLogID = str(dev.idProduct) + '^' + str(dev.idVendor)
-                
-                for line in logData:
-                    logParts = line.split(":")
-                    lineID = logParts[0]
-                
-                    #2. If device is found, display and state that it is registered.
-                    if lineID == DevLogID:
-                        FoundID = True
-                        print(str(dev.product) + ' by '  + str(dev.manufacturer) + ' connected at USB port ' + str(dev.port_number)  + ". Device Already Logged.")
-                        break
-                            
-                    #3. If not, register the device and display it. Noting that is has been saved. The format is= ID:Product:Vendor
-                if not FoundID:
-                    newEntry = DevLogID + ":" + dev.product + ":" + dev.manufacturer + "\n"
-                    logFile.write(newEntry)
-                    print(str(dev.product) + ' by ' + str(dev.manufacturer) + ' connected at USB port ' + str(dev.port_number) + ". Device Now Registered.")
-        #           logData = logFile.readLines()
-                
-                USBCount = USBCount + 1
-
-
-        if not USBCount:
-            print("Nothing connected to USB Ports!")
-    
-    except:
-        raise
-
-    finally:
-        logFile.close()
-
-def CallParamTree(param, devs):
-
-    if param == "-d":
-        displayLog()
-
-    elif param == "-ld":
-        logAndSeeConnectedDevices(devs)
-        displayLog()
-    
-    elif param == "-c":
-        clearLog()
-    
-    elif param == "-dc":
-        displayLog()
-        clearLog()
-    
-    elif param == "-ldc":
-        logAndSeeConnectedDevices(devs)
-        displayLog()
-        clearLog()
-
-#These are the port numbers for the Raspberry Pi 3 Model B
+#These are the USB port numbers for the Raspberry Pi 3 Model B
 PORT_NUMBERS = [2, 3, 4, 5]
 
 #-d is to display everything in the log
@@ -130,13 +35,14 @@ def main():
         if not param in ALLOWED_ARGS:
             print("Parameter entered are not valid.")
         else:
-            CallParamTree(param, devs)
+            USBRegOps.callParamTree(param, devs, PORT_NUMBERS)
         
     except IndexError as e:
         #Seeing as there are no parameters, we want just the basic functionality of adding any
         #unregistered devices to the log
         #Since this is the only time we deal with lists that can cause this error
-        logAndSeeConnectedDevices(devs)
+        #Here we make params = None
+        USBRegOps.callParamTree(None, devs, PORT_NUMBERS)
 
     except ValueError as e:
         print(e)
